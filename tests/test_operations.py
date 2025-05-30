ad10a55c1f0f542fc42299f46ff13f24975d8e55
Om Patel <omsuperson@gmail.com>
2025-05-30 12:53:06 +0530

    Solved a mistake in unit tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,7 +3,7 @@ from src.math_operations import add,sub
 def test_add():
     assert add(2,4) == 6
     assert add(-1,1) == 0
-    assert add(120+34.5) == 154.5
+    assert add(120,34.5) == 154.5
     
 def test_sub():
     assert sub(5,3) == 2
