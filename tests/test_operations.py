a05e266a9455e6de3122a76cf480fc391df36322
Om Patel <omsuperson@gmail.com>
2025-05-30 12:22:33 +0530

    Unit test cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,4) == 6
+    assert add(-1,1) == 0
+    assert add(120+34.5) == 154.5
+    
+def test_sub():
+    assert sub(5,3) == 2
+    assert sub(3,5) == -2
+    
